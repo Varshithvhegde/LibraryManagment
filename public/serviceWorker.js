f5c81e966628c55493ee48c8aa09b72077675836
Varshithvhegde <varshithvh@gmail.com>
2023-05-31 06:35:09 +0530

    Added Service Worker for PWA

---
+++ public/serviceWorker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'my-pwa-cache';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll([
+        '/',
+        '/index.html',
+        '/favicon.png'
+      ]))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
